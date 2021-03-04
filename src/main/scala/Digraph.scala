1575168f46ba163ccabf51e7c3b31cd5969d2068
Dominic Swaine <dominic.swaine@gmail.com>
2021-03-04 00:48:52 +0000

    Implement Digraph.add(...)

--- src/main/scala/Digraph.scala
+++ src/main/scala/Digraph.scala
@@ -5,13 +5,9 @@ case class Digraph[N](nodes: Set[N], edges: Map[(N, N), Float]) {
   def this(nodes: Set[N], edges: List[(N, N, Float)])
     = this(nodes, edges.map(e => ((e._1, e._2), e._3)).toMap)
 
-  def get_edge(from: N, to: N): (N, N, Float)
-    = (from, to, edges(from, to))
-
+  def get_cost(from: N, to: N): Float = edges(from, to)
   def get_incoming(n: N): List[N] = in.getOrElse(n, Nil)
   def get_outgoing(n: N): List[N] = out.getOrElse(n, Nil)
-  def get_incoming_edges(to: N): List[(N, N, Float)] = get_incoming(to).map(from => get_edge(from, to))
-  def get_outgoing_edges(from: N): List[(N, N, Float)] = get_outgoing(from).map(to => get_edge(from, to))
 
   def dijkstra_shortest_path(from: N, to: N): Float
     = Dijkstra.shortest_path(this, from, to)
@@ -19,9 +15,13 @@ case class Digraph[N](nodes: Set[N], edges: Map[(N, N), Float]) {
   def get_paths_through(n: N): List[(N, N)]
     = for(in <- get_incoming(n); out <- get_outgoing(n); if in!=out) yield (in, out)
 
+  def get_costed_paths_through(n: N): List[((N, N), Float)]
+    = get_paths_through(n).map{ case (from, to) => ((from, to), get_cost(from, n) + get_cost(n, to)) }
+
   def remove(n: N): Digraph[N]
     = Digraph(nodes.excl(n), edges.filter{ case k->_ => k._1 != n && k._2 != n })
 
-  def add(e: ((N, N), Float)): Digraph[N] = Digraph(nodes, edges + e)
+  def add(e: ((N, N), Float)): Digraph[N]
+    = Digraph(nodes, edges + e)
 
 }
--- src/main/scala/Dijkstra.scala
+++ src/main/scala/Dijkstra.scala
@@ -10,13 +10,13 @@ object Dijkstra {
       val exp_node = queue.dequeue()
       if(!expanded.contains(exp_node)){
         expanded.add(exp_node)
-        graph.get_outgoing_edges(exp_node).foreach{ case (_, onward_node, edge_cost) =>
-          val new_cost: Float = dist(exp_node) + edge_cost
+        graph.get_outgoing(exp_node).foreach(onward_node => {
+          val new_cost: Float = dist(exp_node) + graph.get_cost(exp_node, onward_node)
           if(!dist.contains(onward_node) || new_cost < dist(onward_node)){
             dist += onward_node -> new_cost
             queue.addOne(onward_node)
           }
-        }
+        })
       }
     }
     dist.getOrElse(to, Float.PositiveInfinity)
