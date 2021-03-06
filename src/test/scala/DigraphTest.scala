c97c5a4fd71119cd613bb1fb3c0bb6b9e73fe700
Dominic Swaine <dominic.swaine@gmail.com>
2021-03-06 19:10:32 +0000

    Refactor DigraphTest

--- src/test/scala/DigraphTest.scala
+++ src/test/scala/DigraphTest.scala
@@ -4,22 +4,28 @@ import org.scalatest.funsuite.AnyFunSuite
 class DigraphTest extends AnyFunSuite {
 
   // Graph as per article on Contraction Hierarchies by Michael Tandy
-  val g1_nodes: Set[Int] = Set(1, 2, 3, 4, 5)
-  val g1_edges: List[(Int, Int, Float)] = List(
-    (1, 3, 1), (3, 1, 1), (2, 3, 4), (3, 2, 4), (2, 4, 1), (4, 2, 1),
-    (3, 5, 1), (5, 3, 1), (4, 5, 1), (5, 4, 1)
-  )
-  val graph: Digraph[Int] = new Digraph(g1_nodes, g1_edges)
+  def get_graph1(): Digraph[Int] = {
+    val g1_nodes: Set[Int] = Set(1, 2, 3, 4, 5)
+    val g1_edges: List[(Int, Int, Float)] = List(
+      (1, 3, 1), (3, 1, 1), (2, 3, 4), (3, 2, 4), (2, 4, 1), (4, 2, 1),
+      (3, 5, 1), (5, 3, 1), (4, 5, 1), (5, 4, 1)
+    )
+    new Digraph(g1_nodes, g1_edges)
+  }
+  val graph: Digraph[Int] = get_graph1()
 
   // Graph as per Monash University slides on Contraction Hierarchies
-  val g2_nodes: Set[Int] = (0 to 11).toSet
-  val g2_undirected_edges: List[(Int, Int, Float)] = List(
-    (1, 2, 1), (1, 6, 1), (1, 10, 1), (3, 5, 1), (3, 11, 1), (4, 5, 1),
-    (4, 11, 2), (7, 8, 1), (7, 9, 1), (9, 10, 1), (9, 11, 1)
-  )
-  val g2_directed_edges: List[(Int, Int, Float)]
+  def get_graph2(): Digraph[Int] = {
+    val g2_nodes: Set[Int] = (0 to 11).toSet
+    val g2_undirected_edges: List[(Int, Int, Float)] = List(
+      (1, 2, 1), (1, 6, 1), (1, 10, 1), (3, 5, 1), (3, 11, 1), (4, 5, 1),
+      (4, 11, 2), (7, 8, 1), (7, 9, 1), (9, 10, 1), (9, 11, 1)
+    )
+    val g2_directed_edges: List[(Int, Int, Float)]
     = g2_undirected_edges ++ g2_undirected_edges.map(e => (e._2, e._1, e._3))
-  val graph2: Digraph[Int] = new Digraph(g2_nodes, g2_directed_edges)
+    new Digraph(g2_nodes, g2_directed_edges)
+  }
+  val graph2: Digraph[Int] = get_graph2()
 
   test("Shortest path in an undirected graph") {
     assert(graph.dijkstra_shortest_path(1, 1) == 0)
@@ -30,9 +36,12 @@ class DigraphTest extends AnyFunSuite {
     assert(graph.dijkstra_shortest_path(1, 6) == Float.PositiveInfinity)
   }
 
-  val g3_nodes: Set[Int] = (1 to 4).toSet
-  val g3_edges: List[(Int, Int, Float)] = List((1, 2, 1), (1, 3, 2), (2, 4, 2), (3, 4, 3))
-  val graph3 = new Digraph[Int](g3_nodes, g3_edges)
+  def get_graph3(): Digraph[Int] = {
+    val g3_nodes: Set[Int] = (1 to 4).toSet
+    val g3_edges: List[(Int, Int, Float)] = List((1, 2, 1), (1, 3, 2), (2, 4, 2), (3, 4, 3))
+    new Digraph[Int](g3_nodes, g3_edges)
+  }
+  val graph3: Digraph[Int] = get_graph3()
 
   test("Shortest path in a directed graph"){
     assert(new Dijkstra[Int](graph3, 1, Direction.Forward).shortest_path(4) == 3)
