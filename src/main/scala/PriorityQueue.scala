6f7386c1e2fca8f0185dce6405be098004baf334
Dominic Swaine <dominic.swaine@gmail.com>
2021-03-15 02:44:40 +0000

    Refactor

--- src/main/scala/PriorityQueue.scala
+++ src/main/scala/PriorityQueue.scala
@@ -1,8 +1,10 @@
 import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 class PriorityQueue[N](implicit val ord: Ordering[N]){
   private val items: ArrayBuffer[N] = ArrayBuffer.empty[N]
+  private val indexes: mutable.Map[N, Int] = mutable.Map.empty[N, Int]
 
   def size: Int = items.size
   def isEmpty: Boolean = items.isEmpty
@@ -28,10 +30,14 @@ class PriorityQueue[N](implicit val ord: Ordering[N]){
   private def right_child(i: Int): N = items(get_right_child_index(i))
   private def parent(i: Int): N = items(get_parent_index(i))
 
-  private def swap(first_index: Int, second_index: Int): Unit = {
+  private def swap(first: N, second: N): Unit = {
+    val first_index: Int = indexes(first)
+    val second_index: Int = indexes(second)
     val temp = items(first_index)
     items(first_index) = items(second_index)
     items(second_index) = temp
+    indexes.addOne(items(first_index) -> first_index)
+    indexes.addOne(items(second_index) -> second_index)
   }
 
   def head: N = {
@@ -42,21 +48,24 @@ class PriorityQueue[N](implicit val ord: Ordering[N]){
   def dequeue(): N = {
     if(isEmpty){throw new IllegalStateException()}
     val head = items.head
-    items(0) = items(size - 1)
-    items.remove(size - 1)
+    swap(items(0), items(size-1))
+    items.remove(size-1)
+    indexes.remove(head)
     heapify_down(0)
     head
   }
 
   def enqueue(element: N): Unit = {
     items.addOne(element)
-    heapify_up(size - 1)
+    indexes.addOne(element -> (size-1))
+    heapify_up(element)
   }
 
-  @tailrec private def heapify_up(index: Int): Unit = {
+  @tailrec private def heapify_up(element: N): Unit = {
+    val index: Int = indexes(element)
     if(has_parent(index) && ord.gt(parent(index), items(index))){
-      swap(get_parent_index(index), index)
-      heapify_up(get_parent_index(index))
+      swap(parent(index), items(index))
+      heapify_up(parent(index))
     }
   }
 
@@ -64,7 +73,7 @@ class PriorityQueue[N](implicit val ord: Ordering[N]){
     if(has_left_child(index)){
       val smaller_child_index: Int = get_smaller_child_index(index)
       if(ord.gteq(items(index), items(smaller_child_index))){
-        swap(index, smaller_child_index)
+        swap(items(index), items(smaller_child_index))
         heapify_down(smaller_child_index)
       }
     }
